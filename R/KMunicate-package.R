5bb921d3d6b0c2735cd95ea1926f360d8236becb
Alessandro Gasparini <ellessenne@gmail.com>
2020-07-10 12:28:22 +0200

    Implemented KMunicate function

---
+++ R/KMunicate-package.R
@@ -0,0 +1,12 @@
+#' @title KMunicate-Style Kaplan–Meier Plots
+#'
+#' @description Produce Kaplan–Meier plots in the style recommended following the KMunicate study <doi:10.1136/bmjopen-2019-030215>.
+#'
+#' @name KMunicate
+#' @docType package
+#' @author Alessandro Gasparini (alessandro.gasparini@@ki.se)
+#' @import cowplot dplyr ggfortify ggplot2 pammtools survival tidyr
+NULL
+
+# Quiets concerns of R CMD check re: variable names used internally
+if (getRversion() >= "2.15.1") utils::globalVariables(c("time", "surv", "lower", "upper", "strata", "table_group", "n.event", "n.censor", "n.risk", "name", "value"))
---
+++ R/KMunicate.R
@@ -0,0 +1,79 @@
+#' @title KMunicate-Style Kaplan–Meier Plots
+#' @description Produce Kaplan–Meier plots in the style recommended following the KMunicate study <doi:10.1136/bmjopen-2019-030215>.
+#' @param fit A `survfit` object.
+#' @param time_scale The time scale that will be used for the x-axis and for the summary tables.
+#' @param .theme `ggplot` theme used by the plot. Defaults to [ggplot2::theme_minimal()].
+#' @param .color_scale Colour scale used for the plot. Has to be a `scale_colour_*` component, and defaults to `NULL` where the default colour scales will be used.
+#' @param .fill_scale Fill scale used for the plot. Has to be a `scale_fill_*` component, and defaults to `NULL` where the default fill scales will be used.
+#' @param .xlab Label for the horizontal axis, defaults to _Time_.
+#' @param .alpha Transparency of the point-wise confidence intervals
+#' @param .rel_heights Override default relative heights of plots and tables. See [cowplot::plot_grid()] for more details on how to use this argument.
+#' @return A KMunicate-style `ggplot` object.
+#' @export
+#'
+#' @examples
+#' library(survival)
+#' data("cancer", package = "KMunicate")
+#' KM <- survfit(Surv(studytime, died) ~ drug, data = cancer)
+#' time_scale <- seq(0, max(cancer$studytime), by = 7)
+#' KMunicate(fit = KM, time_scale = time_scale)
+KMunicate <- function(fit, time_scale, .theme = ggplot2::theme_minimal(), .color_scale = NULL, .fill_scale = NULL, .xlab = "Time", .alpha = 0.25, .rel_heights = NULL) {
+
+  ### Fortify data
+  data <- ggplot2::fortify(fit)
+  data_zero <- data.frame(time = 0, n.risk = fit$n, n.event = 0, n.censor = 0, surv = 1, std.err = 0, upper = 1, lower = 1, strata = unique(data$strata))
+  data <- rbind.data.frame(data, data_zero)
+
+  ### Create plot
+  plot <- ggplot2::ggplot(data, ggplot2::aes(x = time, y = surv)) +
+    pammtools::geom_stepribbon(ggplot2::aes(ymin = lower, ymax = upper, fill = strata), alpha = .alpha) +
+    ggplot2::geom_step(ggplot2::aes(color = strata, linetype = strata)) +
+    ggplot2::scale_x_continuous(breaks = time_scale) +
+    ggplot2::coord_cartesian(ylim = c(0, 1), xlim = range(time_scale)) +
+    ggplot2::labs(color = "", fill = "", linetype = "", x = .xlab, y = "Estimated survival") +
+    .theme +
+    ggplot2::theme(legend.position = c(1, 1), legend.justification = c(1, 1), legend.background = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank())
+  if (!is.null(.color_scale)) {
+    plot <- plot + .color_scale
+  }
+  if (!is.null(.fill_scale)) {
+    plot <- plot + .fill_scale
+  }
+
+  ### Create tables
+  data$table_group <- findInterval(x = data$time, vec = time_scale, left.open = TRUE)
+  table_data <- dplyr::group_by(data, strata, table_group)
+  table_data <- dplyr::summarise(table_data,
+    events = sum(n.event),
+    censor = sum(n.censor),
+    at_risk = max(n.risk)
+  )
+  table_data <- dplyr::ungroup(table_data)
+  table_data <- dplyr::left_join(table_data, data.frame(time_scale, table_group = seq_along(time_scale) - 1), by = "table_group")
+  table_data$table_group <- NULL
+  table_data <- tidyr::pivot_longer(data = table_data, cols = c("events", "censor", "at_risk"))
+  table_data$name <- factor(table_data$name, levels = c("events", "censor", "at_risk"), labels = c("Events", "Censored", "At risk"))
+
+  ### Create table 'plots'
+  tds <- split(table_data, f = table_data$strata)
+  tds <- lapply(seq_along(tds), function(i) {
+    ggplot2::ggplot(tds[[i]], ggplot2::aes(x = time_scale, y = name, label = value)) +
+      ggplot2::geom_text() +
+      ggplot2::scale_x_continuous(breaks = time_scale) +
+      ggplot2::coord_cartesian(xlim = range(time_scale)) +
+      ggplot2::theme_void() +
+      ggplot2::theme(axis.text.y = ggplot2::element_text(face = "italic")) +
+      ggplot2::labs(title = names(tds)[i])
+  })
+
+  ### Process relative heights
+  if (is.null(.rel_heights)) {
+    .rel_heights <- c(3, rep(1, length(tds)))
+  }
+
+  ### Combine plot and tables
+  KMunicate_plot <- cowplot::plot_grid(plotlist = c(list(plot), tds), align = "hv", axis = "tlbr", ncol = 1, rel_heights = .rel_heights)
+
+  ### Return
+  return(KMunicate_plot)
+}
