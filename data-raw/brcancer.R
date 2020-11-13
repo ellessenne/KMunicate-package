1923e99fe62cf5e535fbeb942332788ab5800c42
Alessandro Gasparini <ellessenne@gmail.com>
2020-11-13 15:48:20 +0100

    Updated bundled datasets

--- data-raw/brcancer.R
+++ data-raw/brcancer.R
@@ -1,4 +1,9 @@
 ## code to prepare `brcancer` dataset goes here
 library(haven)
 brcancer <- haven::read_dta(file = "http://www.stata-press.com/data/r16/brcancer.dta")
+brcancer <- haven::zap_formats(brcancer)
+brcancer <- haven::zap_label(brcancer)
+brcancer <- haven::zap_labels(brcancer)
+brcancer <- haven::zap_missing(brcancer)
+brcancer <- haven::zap_widths(brcancer)
 usethis::use_data(brcancer, overwrite = TRUE)
--- data-raw/cancer2.R
+++ data-raw/cancer2.R
@@ -1,4 +1,9 @@
 ## code to prepare `cancer` dataset goes here
 library(haven)
 cancer2 <- haven::read_dta(file = "http://www.stata-press.com/data/r16/cancer.dta")
+cancer2 <- haven::zap_formats(cancer2)
+cancer2 <- haven::zap_label(cancer2)
+cancer2 <- haven::zap_labels(cancer2)
+cancer2 <- haven::zap_missing(cancer2)
+cancer2 <- haven::zap_widths(cancer2)
 usethis::use_data(cancer2, overwrite = TRUE)
