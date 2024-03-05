7f1470145bf262efa0c9d00e91a698f32eec2fa7
Alessandro Gasparini <git@ellessenne.xyz>
2024-03-05 22:21:13 +0100

    Housekeeping

--- R/KMunicate-package.R
+++ R/KMunicate-package.R
@@ -1,12 +1,9 @@
-#' @title KMunicate-Style Kaplan–Meier Plots
-#'
-#' @description Produce Kaplan–Meier plots in the style recommended following the KMunicate study, \doi{10.1136/bmjopen-2019-030215}.
-#'
-#' @docType package
-#' @name KMunicate-package
-#' @aliases KMunicate-package
-#' @author Alessandro Gasparini (alessandro@@ellessenne.xyz)
 #' @import cowplot ggplot2 pammtools survival tidyr
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
 
 # Quiets concerns of R CMD check re: variable names used internally
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# KMunicate
+# KMunicate-Style Kaplan–Meier Plots
 
 <!-- badges: start -->
 
